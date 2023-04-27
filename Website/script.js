7bae56de857a4c8cac2b939f6f5eb1144c267242
TomSoerr <tomix2004@gmail.com>
2023-04-27 19:50:06 +0200

    Add a carrousel slider

---
+++ Website/script.js
@@ -0,0 +1,28 @@
+// ferienfreizeiten > segeln und biken > carrousel slider
+
+const slider = document.querySelector("#ff-sub-carrousel-wrapper");
+const leftArrow = document.querySelector("#ff-sub-carrousel-left");
+const rightArrow = document.querySelector("#ff-sub-carrousel-right");
+let sliderSliding = false;
+
+rightArrow.addEventListener("click", () => {
+  sliderSliding = true;
+  slider.classList.add("right");
+  const first = slider.firstElementChild;
+  setTimeout(() => {
+    slider.appendChild(first);
+    slider.classList.remove("right");
+    sliderSliding = false;
+  }, 505);
+});
+
+leftArrow.addEventListener("click", () => {
+  slider.classList.add("left");
+  const first = slider.firstElementChild;
+  const last = slider.lastElementChild;
+  setTimeout(() => {
+    slider.insertBefore(last, first);
+    slider.classList.remove("left");
+    sliderSliding = false;
+  }, 505);
+});
